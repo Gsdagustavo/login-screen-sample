418cad4bad5da7e3c4259db16babb74b4d50e6d3
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-03 08:26:20 -0300

    made the LoginPage widget

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       /// Defines the app's title
       title: 'Login',
       debugShowCheckedModeBanner: false,
 
       /// Defines the default theme data
       theme: ThemeData(brightness: Brightness.light),
+
+      routes: {'/loginPage': (_) => const LoginPage()},
     );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Hello World!')));
   }
 }
