801a9caa32c3134c8ee32a04047ec5cf7abccee4
Gsdagustavo <gugadanielalvez@gmail.com>
2025-06-03 08:23:46 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+/// Entry point for running the [Main App]
+void main() {
+  runApp(const MyApp());
+}
+
+/// [Main App]
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      /// Defines the app's title
+      title: 'Login',
+      debugShowCheckedModeBanner: false,
+
+      /// Defines the default theme data
+      theme: ThemeData(brightness: Brightness.light),
+    );
+  }
+}
